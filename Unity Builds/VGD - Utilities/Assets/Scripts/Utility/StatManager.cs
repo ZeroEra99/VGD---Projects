b4d2a9c2a8befbdd6094428c16d4ba1a45d6b086
Ceredril <l.rienks@studenti.unica.it>
2022-11-29 19:40:53 +0100

    integrated last Checkpoint into PlayerStat and the HUD

---
+++ Unity Builds/VGD - Utilities/Assets/Scripts/Utility/StatManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setHealth(int newHealth)
+    {
+
+    }
+    void decreaseHealth(int healthReduction)
+    {
+
+    }
+
+    void setMana(int newMana)
+    {
+
+    }
+    void decreaseMana(int manaReduction)
+    {
+
+    }
+
+    void setStamina(int newStamina)
+    {
+
+    }
+    void decreaseStamina(int staminaReduction)
+    {
+
+    }
+
+}
